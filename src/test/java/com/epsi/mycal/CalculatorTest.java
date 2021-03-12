158906224e75c9bf3b64bf94edd46d71f5c491a2
Nicolas Piveteau <nicolas.piveteau@epsi.fr>
2021-03-12 08:29:24 +0100

    scd

---
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.epsi.mycal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 1;
+        int b = 3;
+        Calculator calc = new Calculator();
+        int actual = calc.add(a, b);
+        assertEquals(4, actual);
+    }
+
+    @Test
+    public void testSub() {
+        int a = 1;
+        int b = 3;
+        Calculator calc = new Calculator();
+        int actual = calc.sub(a, b);
+        assertEquals(-2, actual);
+    }
+}
